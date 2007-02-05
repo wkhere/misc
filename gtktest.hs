cc99ec88e361874fea7001d991b40374afd3992f
Wojciech Kaczmarek <wk@dual-tech.com.pl>
2007-02-05 21:22:44 +0100

    gtktest: more experiments
    darcs-hash:20070205202244-ee2dc-7602fa04b2fff17cf860059567c51bc61f9d0250.gz

--- gtktest.hs
+++ gtktest.hs
@@ -7,14 +7,16 @@ main :: IO ()
 main = do
   initGUI
   Just xml <- xmlNew gladeFile
-  w <- xmlGetWidget xml castToWindow "window1"
+  let getw = xmlGetWidget xml castToWindow
+  let getb = xmlGetWidget xml castToButton
+  w <- getw "window1"
   onDestroy w mainQuit
-  clBtn <- xmlGetWidget xml castToButton "closeButton"
+  clBtn <- getb "closeButton"
   onClicked clBtn $ do
          widgetDestroy w
   mainGUI
 
 {- TODO:
- - check why initGUI fails in ghci on mac
+ - check why initGUI fails in ghci on mac (it doesn't on debian x86)
  - check "insanity" of glade
 -}
