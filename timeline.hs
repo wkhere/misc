6f97072a992afe6252b20fbf308edae8c118e280
Wojciech Kaczmarek <wk@dual-tech.com.pl>
2007-05-20 20:19:06 +0200

    timeline: init version
    darcs-hash:20070520181906-ee2dc-75ac2e581741bb488444997816d47e43813f4662.gz

---
+++ timeline.hs
@@ -0,0 +1,36 @@
+module Main where
+import Text.ParserCombinators.Parsec
+import Text.Printf
+
+eot = eof >> return ' '
+--eol = newline <|> (eof >> return ' ')
+--tilEol = manyTill (noneOf "\n") eol 
+
+p = do
+  dateS <- manyTill (noneOf ":") (char ':')
+  skipMany $ oneOf " \t"
+  t11 <- count 2 digit
+  t12 <- count 2 digit
+  char '-'
+  t21 <- count 2 digit 
+  t22 <- count 2 digit 
+  char '=' <|> space <|> eot
+  return $ concat [dateS, ": ", t11, t12, "-", t21, t22, " = ", 
+                   hrDesc (toMins t11 t12) (toMins t21 t22),
+                   "h"]
+    where
+      toMins c1 c2 = (read c1::Integer)*60 + (read c2::Integer)
+      hrDesc t1 t2 = show (fromInteger(tmDiff t1 t2)/60)
+          where
+            tmDiff t1 t2 = if t1 < t2 then t2-t1
+                           else t2+24*60-t1
+
+doParseLine = runParser p () ""
+
+procLine :: String -> String
+procLine s =
+    case (doParseLine s) of
+      Left e -> s -- ignore the error, output line as it was
+      Right s1 -> s1
+      
+main = interact (unlines . (map procLine) . lines)
