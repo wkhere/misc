a73f0109db15f7f430a61245a2e0a628dcdd30a1
Wojciech Kaczmarek <wk@dual-tech.com.pl>
2008-02-09 20:25:50 +0100

    csv.erl: add support for line-by-line and stdin processing
    darcs-hash:20080209192550-ee2dc-9d5bffb8680c6efd8054191698d735ecd1b375b4.gz

--- csv.erl
+++ csv.erl
@@ -1,6 +1,7 @@
 -module(csv).
 -import(lists, [reverse/1]).
--export([parse/1, parse/2]).
+-export([parse/1, parse/2, 
+	 parse_by_line/1, parse_by_line/2]).
 %-compile(export_all). %tmp
 -record(opts, {sep, quote, token_trans}).
 -define(default_quote, $\").
@@ -19,12 +20,43 @@ complement_opts(Opts) ->
 parse(X) ->
     parse(X, #opts{}).
 
+parse(IO=standard_io, Opts) ->
+    io:setopts([binary]),
+    parse_by_line(IO, Opts);
+
+parse(IO, Opts) when is_pid(IO) ->
+    parse_by_line(IO, Opts);
+
 parse(Filename, Opts) when is_list(Filename) ->
     {ok, Bin} = file:read_file(Filename),
     parse(Bin, Opts);
 
 parse(Bin, Opts) when is_binary(Bin) ->
     parse(Bin, [], [], [], complement_opts(Opts)).
+
+
+parse_by_line(X) ->
+    parse_by_line(X, #opts{}).
+
+parse_by_line(Filename, Opts) when is_list(Filename) ->
+    {ok, IO} = file:open(Filename, [read, binary]),
+    Res = parse_by_line(IO, Opts),
+    ok = file:close(IO),
+    Res;
+
+parse_by_line(IO, Opts) ->
+    OptFull = complement_opts(Opts),
+    F = fun(F0, Acc) -> 
+		case X = io:get_line(IO,'') of 
+		    _ when X=:=eof; X=:=<<"EOF\n">> ->
+			reverse(Acc);
+		    _ ->
+			{ok, [OneLine]} = parse(X, [], [], [], OptFull),
+			F0(F0, [OneLine|Acc])
+		end
+	end,
+    {ok, F(F, [])}.
+
 
 parse(<<Sep, Rest/binary>>, Field, Line, Acc, 
       #opts{sep=Sep, token_trans=F} = Opts) ->
