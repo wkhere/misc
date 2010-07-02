b0c3fcd28de22b4542100bc9907fb09cffb360bf
Wojciech Kaczmarek <wojtekk@kofeina.net>
2010-07-02 18:16:50 +0200

    soe: t1: remove double space keystroke needed to close window

--- soe/t1.hs
+++ soe/t1.hs
@@ -14,7 +14,6 @@ runHello = runGraphics $ do
              w <- openWindow ("SOE t1 @" ++ host) (400, 300)
              drawInWindow w (text (20, 100) 
                                $ "Hello! It works at the host: " ++ host)
-             k <- getKey w
              spaceClose w
 
 fillTri :: Window -> Int -> Int -> Int -> IO ()
