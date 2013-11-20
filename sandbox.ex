f7d3931272a25712c768bfc073a6c31e0a110e87
Wojciech Kaczmarek <frk@kofeina.net>
2013-11-20 12:57:17 +0100

    Elixir sandbox: fix indent

--- sandbox.ex
+++ sandbox.ex
@@ -1,14 +1,14 @@
 defmodule Sandbox do
-    def test_default_arg(a // 42) do {a} end
-    def id(a) do a end
-    def test_bind(a) do
-        x = 111
-        binding
-    end
+  def test_default_arg(a // 42) do {a} end
+  def id(a) do a end
+  def test_bind(a) do
+    x = 111
+    binding
+  end
 
-    defmacro mirror(body) do
-        quote do 
-            unquote body
-        end
+  defmacro mirror(body) do
+    quote do
+      unquote body
     end
+  end
 end
